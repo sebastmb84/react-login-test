f9b8fcb0cf52e92704d4493e3b17b7b035ae426a
sbmol <sebastianmolinasb@gmail.com>
2021-12-06 19:33:22 -0300

    1st day react-native

--- App.js
+++ App.js
@@ -2,13 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import loginScreen from './screens/loginScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-      <h3 className="text-white pt-2">Hola Soy mi primera App</h3>
+      <loginScreen></loginScreen>
     </View>
   );
 }
---
+++ screens/loginScreen.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+class loginScreen extends React.Component {
+
+    render() {
+        return (
+            <View>
+                
+                <p className="text-white">Soy una Vista desde Login Screen</p>
+            </View>
+        )
+    }
+
+}
+
+export default loginScreen; 
+
