be0354479ec52e67fea112c77f5e9837dd300579
sbmol <sebastianmolinasb@gmail.com>
2021-12-06 18:10:17 -0300

    first update

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <h3 className="text-white pt-2">Hola Soy mi primera App</h3>
     </View>
   );
 }
@@ -14,7 +16,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f34022',
     alignItems: 'center',
     justifyContent: 'center',
   },
